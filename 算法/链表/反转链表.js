0503c236865f7949cd55e28e977136e39c7e854e
majuntao <1547996098@qq.com>
2024-11-14 22:02:13 +0800

    反转链表

---
+++ 算法/链表/反转链表.js
@@ -0,0 +1,22 @@
+import {arrayToLinkedList, ListNode} from "./构造链表.js";
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let prev = null;
+    let current = head;
+
+    while (current) {
+        const nextNode = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev; // 返回新的头节点
+};
+const head = [1, 2, 3, 4, 5]
+const p = arrayToLinkedList(head)
+reverseList(p)
